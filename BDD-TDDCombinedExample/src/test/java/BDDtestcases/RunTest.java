3f607b126db976b09ef291da407b73f9c91a1ed6
Shreyata <shreyata_suanghi@hotmail.com>
2026-02-12 02:08:01 +0530

    Updated files

--- BDD-TDDCombinedExample/src/test/java/BDDtestcases/RunTest.java
+++ BDD-TDDCombinedExample/src/test/java/BDDtestcases/RunTest.java
@@ -1,13 +1,23 @@
 package BDDtestcases;
 
-import org.junit.platform.suite.api.*;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectClasspathResource("features")  
 @ConfigurationParameter(
-    key = "cucumber.plugin",
-    value = "pretty, html:target/cucumber-report.html"
+        key = GLUE_PROPERTY_NAME,
+        value = "BDDtestcases"        
+)
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:target/cucumber-report.html"
 )
 public class RunTest {
 }
