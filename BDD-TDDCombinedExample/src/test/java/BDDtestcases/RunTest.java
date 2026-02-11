7e1222964a394b1c210b2906e5ad753c4d4142d5
Shreyata <shreyata_suanghi@hotmail.com>
2026-02-12 01:37:37 +0530

    Updated files for updated version

--- BDD-TDDCombinedExample/src/test/java/BDDtestcases/RunTest.java
+++ BDD-TDDCombinedExample/src/test/java/BDDtestcases/RunTest.java
@@ -1,14 +1,15 @@
 package BDDtestcases;
 
-import io.cucumber.junit.platform.engine.Constants;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "BDDtestcases")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepdefinitions")
 public class RunTest {
 }
